cbb1e5f04772de91544bd745c2ff16a5c67d2b76
m1ji <112436262+m1ji@users.noreply.github.com>
2022-10-13 10:54:20 -0400

    that door opens quite fast

--- Assets/LearningUnits/Unit02/Scripts/ThisAndThat.cs
+++ Assets/LearningUnits/Unit02/Scripts/ThisAndThat.cs
@@ -28,6 +28,9 @@ public class ThisAndThat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isBlueCubeDeteced && isRedCubeDetected)
+        { areBothCubesDetected = true; }
+        
         if (areBothCubesDetected)
         {
             door.GetComponent<MoveToLocation>().enabled = true;
@@ -36,14 +39,14 @@ public class ThisAndThat : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //if (other.gameObject something something)
-        //{
-        //    isBlueCubeDeteced = true;
-        //}
+        if (other.gameObject == blueCube)
+        {
+            isBlueCubeDeteced = true;
+        }
 
-        //if (other.gameObject something something)
-        //{
-        //    isRedCubeDetected = true;
-        //}
+        if (other.gameObject == redCube)
+        {
+            isRedCubeDetected = true;
+        }
     }
 }
